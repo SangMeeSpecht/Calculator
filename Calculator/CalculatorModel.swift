a2e409216df8c2e18984771a04ee6b8d889f72e8
SangMee Specht <spechtse@miamioh.edu>
2017-03-31 16:00:03 -0500

    Add multiplication functionality

--- Calculator/CalculatorModel.swift
+++ Calculator/CalculatorModel.swift
@@ -8,18 +8,32 @@
 
 import Foundation
 
+func multiply(factor1: Double, factor2: Double) -> Double {
+    return factor1 * factor2
+}
+
 class CalculatorModel {
     
     private var accumulator = 0.0
     
+    var result: Double {
+        get {
+            return accumulator
+        }
+    }
+    
     private var operations = [
         "π" : Operation.Constant(Double.pi),
-        "√" : Operation.UnaryOperation(sqrt)
+        "√" : Operation.UnaryOperation(sqrt),
+        "×" : Operation.BinaryOperation(multiply),
+        "=" : Operation.Equals
     ]
     
     private enum Operation {
         case Constant(Double)
         case UnaryOperation((Double) -> Double)
+        case BinaryOperation((Double, Double) -> Double)
+        case Equals
     }
     
     func setOperand(operand: Double) {
@@ -31,15 +45,22 @@ class CalculatorModel {
             switch operation {
             case .Constant(let value): accumulator = value
             case .UnaryOperation(let function): accumulator = function(accumulator)
+            case .BinaryOperation(let function): pending = PendingBinaryOperationInfo(binaryFunction: function, firstOperand: accumulator)
+            case .Equals:
+                if pending != nil {
+                    accumulator = pending!.binaryFunction(pending!.firstOperand, accumulator)
+                    pending = nil 
+                }
+                
             }
         }
-        
     }
     
-    var result: Double {
-        get {
-            return accumulator
-        }
+    private var pending: PendingBinaryOperationInfo?
+    
+    struct PendingBinaryOperationInfo {
+        var binaryFunction: (Double, Double) -> Double
+        var firstOperand: Double
     }
     
-}   
+}
