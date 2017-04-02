d9214a74deb6da31612347f049cf3e30cce41a57
SangMee Specht <spechtse@miamioh.edu>
2017-04-01 21:15:27 -0500

    Add cubed button and functionality

--- Calculator/CalculatorModel.swift
+++ Calculator/CalculatorModel.swift
@@ -46,6 +46,7 @@ class CalculatorModel {
         "√" : Operation.UnaryOperation(sqrt),
         "x!": Operation.UnaryOperation(factorial),
         "x²": Operation.UnaryOperation({ $0 * $0 }),
+        "x³": Operation.UnaryOperation({ $0 * $0 * $0 }),
         "±" : Operation.UnaryOperation({ -$0 }),
         "×" : Operation.BinaryOperation({ $0 * $1 }),
         "÷" : Operation.BinaryOperation({ $0 / $1 }),
