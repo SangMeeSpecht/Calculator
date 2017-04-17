ff457f97273af5dfa4e2873fd0ebea721cea8fb5
SangMee Specht <spechtse@miamioh.edu>
2017-04-17 18:41:49 -0500

    [Project 3] add pan gesture handler for graph

--- Calculator/GraphView.swift
+++ Calculator/GraphView.swift
@@ -10,18 +10,22 @@ import UIKit
 
 @IBDesignable
 class GraphView: UIView {
-    private var origin: CGPoint {
-        get {
-            return CGPoint(x: bounds.midX, y: bounds.midY)
-        }
-    }
-    
-    let graph = AxesDrawer()
+    @IBInspectable
+    private var origin = CGPoint(x: 0, y: 0){ didSet{ setNeedsDisplay() } }
     
     @IBInspectable
-    var scale: CGFloat = 50.0 { didSet { setNeedsDisplay() } }
+    private var scale: CGFloat = 50.0 { didSet { setNeedsDisplay() } }
+    
+    private var newGraph = true
+    
+    private let graph = AxesDrawer()
     
     override func draw(_ rect: CGRect) {
+        if newGraph {
+            setStartingOrigin()
+            newGraph = false
+        }
+        
         graph.drawAxes(in: rect, origin: origin, pointsPerUnit: scale)
     }
     
@@ -36,7 +40,21 @@ class GraphView: UIView {
     }
     
     func pan(_ recognizer: UIPanGestureRecognizer) {
-
+        if recognizer.state == .ended || recognizer.state == .changed {
+            let distancePanned = recognizer.translation(in: self)
+            
+            setNewOrigin(withDistance: distancePanned)
+            
+            recognizer.setTranslation(CGPoint.zero, in: self)
+        }
     }
 
+    private func setStartingOrigin() {
+        origin.x = bounds.midX
+        origin.y = bounds.midY
+    }
+    
+    private func setNewOrigin(withDistance distancePanned: CGPoint) {
+        origin = CGPoint(x: distancePanned.x + origin.x, y: distancePanned.y + origin.y)
+    }
 }
